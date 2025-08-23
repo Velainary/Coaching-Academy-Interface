1a4de44f7f04d508589ad080d87722b5079e9813
Velainary <155464281+Velainary@users.noreply.github.com>
2025-08-23 21:17:17 +0530

    more work

--- script.js
+++ script.js
@@ -28,3 +28,13 @@ scrollContainers.forEach(container => {
     container.scrollLeft = scrollLeft - walk;
   });
 });
+const toggleBtn = document.getElementById("theme-toggle");
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+
+  if (document.body.classList.contains("dark-mode")) {
+    toggleBtn.textContent = "☀️ Light Mode";
+  } else {
+    toggleBtn.textContent = "🌙 Dark Mode";
+  }
+});
